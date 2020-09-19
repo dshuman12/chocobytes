b21a1615e3e5502fb1e43cb5ae2d5ba452e34b48
dshuman12 <30298537+dshuman12@users.noreply.github.com>
2020-09-19 19:07:38 -0400

    Added search

--- src/App.js
+++ src/App.js
@@ -1,37 +1,62 @@
 import React from 'react'
-import ReactDOM, { render } from 'react-dom'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import "leaflet/dist/leaflet.css";
+import ReactLeafletSearch from "react-leaflet-search";
+import 'index.css'
+import L from 'leaflet'
+import { BsFillHouseFill } from 'react-icons/bs';
 
-class SimpleExample extends React.Component {
+class FullMap extends React.Component {
   constructor() {
     super()
     this.state = {
       lat: 51.505,
       lng: -0.09,
       zoom: 13,
-    minZoom: 2
+    minZoom: 2, 
+     
       }
     }
 
   render() {
     const position = [this.state.lat, this.state.lng];
+
+    const myIcon = L.icon({
+        iconUrl: "home.png",    
+        iconRetinaUrl: "home.png",  
+        iconSize: [40, 40],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        tooltipAnchor: [16, -28],
+        shadowSize: [41, 41]
+    });
+
     return (
         <div>
-      <Map center={position} zoom={this.state.zoom}
-      style={{ width: "100%", position: "absolute", top: 0, bottom: 0, zIndex: 500, }}
-      updateWhenZooming={false}
-      updateWhenIdle={true}
-      preferCanvas={true}
-      minZoom={this.state.minZoom}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
-        />
-      </Map>
-      </div>
+            <Map center={position} zoom={this.state.zoom}
+            style={{ width: "100%", position: "absolute", top: 0, bottom: 0, zIndex: 500, }}
+            updateWhenZooming={false}
+            updateWhenIdle={true}
+            preferCanvas={true}
+            minZoom={this.state.minZoom}>
+                <ReactLeafletSearch 
+                    className="custom-style"
+                    position="topleft" 
+                    provider="OpenStreetMap" 
+                    providerOptions={{ region: "gb" }}
+                    inputPlaceholder="The default text in the search bar"
+                    showMarker={true}
+                    showPopup={false}
+                    markerIcon={myIcon}>
+                </ReactLeafletSearch>
+                <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                />
+            </Map>
+        </div>
     );
   }
 }
 
-export default SimpleExample;
+export default FullMap;
